266eacdb95ee45dfbdb04da1958242b98764889a
l.gil <l.gil@AR-IT10026>
2018-05-29 14:30:33 -0300

    auth update

--- settings.js
+++ settings.js
@@ -117,14 +117,14 @@ module.exports = {
     // -----------------
     // To password protect the Node-RED editor and admin API, the following
     // property can be used. See http://nodered.org/docs/security.html for details.
-    adminAuth: {
-       type: "credentials",
-       users: [{
-           username: process.env.ADMIN_USERNAME,
-           password: process.env.ADMIN_PASSWORD,
-           permissions: "*"
-       }]
-    },
+    // adminAuth: {
+    //    type: "credentials",
+    //    users: [{
+    //        username: process.env.ADMIN_USERNAME,
+    //        password: process.env.ADMIN_PASSWORD,
+    //        permissions: "*"
+    //    }]
+    // },
 
     // To password protect the node-defined HTTP endpoints (httpNodeRoot), or
     // the static content (httpStatic), the following properties can be used.
